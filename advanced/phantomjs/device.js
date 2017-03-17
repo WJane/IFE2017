d33cb49f5a4e47cf0aa489805be259bb97ff6f99
Jane Wu <1198829740@qq.com>
2017-03-17 17:16:20 +0800

    phantom2

---
+++ advanced/phantomjs/device.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'iPhone 5': {
+    width: 320,
+    height: 568,
+    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+  },
+
+  'iPhone 6': {
+    width: 375,
+    height: 667,
+    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+  },
+
+  'ipad': {
+    width: 768,
+    height: 1024,
+    userAgent: 'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+  }
+}
+
+
