fc9d2d6f277a4301acba9b7b604f2c6804a67cc6
Jane Wu <1198829740@qq.com>
2017-04-19 21:05:34 +0800

    do function

--- advanced/formular calculator/src/index.js
+++ advanced/formular calculator/src/index.js
@@ -2,13 +2,54 @@
   const content = document.getElementById('content')
   const count = document.getElementById('count')
   const show = document.getElementById('show')
+
+  //比较元素优先级
+  function compare (ele, cur) {
+    let temp = {}
+    function convert (tar) {
+      if (tar == '+' || tar == '-') {
+        temp.tar = 0
+      } else {
+        temp.tar = 1
+      }
+    }
+    temp(ele)
+    temp(cur)
+    //优先级低于栈顶符号，出栈
+    if (temp.ele<temp.cur) {
+      return -1
+    } else if(temp.ele>temp.cur) {
+      return 1
+    } else {
+      return 0
+    }
+
+  }
   count.onclick = function () {
     let current = content.value.trim()
+    //优先级规则
+
     //中缀表达式转化为后缀表达式
     let stack = []
+    let suffixExp = ''
     for (let v of current) {
-      if (/[1-9]/.exec(v))
+      //若是数字，则进栈
+      if (/\d/.exec(v)){
+        suffixExp+=v
+
+        //若是符号
+      } else {
+        let top = stack.pop()   //栈顶元素
+        //是右括号或优先级低于栈顶符号（乘除优先加减）则栈顶元素依次出栈并输出
+        if (v==')'||compare(top, v)==-1) {
+          suffixExp+=top
+        }
+
+      }
     }
   }
 
 })();
+
+
+
