d8572ebf8677526e92cca55a3dbec5b0dcaaf845
Jane Wu <1198829740@qq.com>
2017-04-21 20:44:26 +0800

    to suffix

--- advanced/formular calculator/src/index.js
+++ advanced/formular calculator/src/index.js
@@ -3,53 +3,60 @@
   const count = document.getElementById('count')
   const show = document.getElementById('show')
 
-  //比较元素优先级
-  function compare (ele, cur) {
-    let temp = {}
-    function convert (tar) {
-      if (tar == '+' || tar == '-') {
-        temp.tar = 0
-      } else {
-        temp.tar = 1
-      }
-    }
-    temp(ele)
-    temp(cur)
-    //优先级低于栈顶符号，出栈
-    if (temp.ele<temp.cur) {
-      return -1
-    } else if(temp.ele>temp.cur) {
-      return 1
-    } else {
-      return 0
-    }
-
-  }
   count.onclick = function () {
     let current = content.value.trim()
-    //优先级规则
+    //9+(3-1)*3+10/2  ==> 9 3 1- 3 * + 10 2 / +
 
     //中缀表达式转化为后缀表达式
     let stack = []
     let suffixExp = ''
     for (let v of current) {
-      //若是数字，则进栈
+      //若是数字，则输出
       if (/\d/.exec(v)){
         suffixExp+=v
-
         //若是符号
       } else {
-        let top = stack.pop()   //栈顶元素，出栈
-        //是右括号或优先级低于栈顶符号（乘除优先加减）则栈顶元素依次出栈并输出
-        while (v==')'||compare(top, v)==-1) {
-          suffixExp += top
-          stack.unshift(v)
-          top = stack.pop()
+        if(stack.length==0||v=='('){
+          stack.push(v)
+        }else{
+          let top = stack[stack.length-1]
+          if(v==")"){
+            while(stack.length>0){
+              let tmp = stack.pop()
+              if(tmp=='('){
+                break;
+              }else {
+                suffixExp+=tmp
+              }
+            }
+          }else if(top=='*'||top=='/'){
+
+            if(v=='*'||v=='/'){
+
+              suffixExp+=v
+            }else{
+              while(stack.length>0){
+                tmp = stack.pop()
+                suffixExp+=tmp
+              }
+              stack.push(v)
+            }
+          }else{
+            stack.push(v)
+          }
         }
-
-
       }
     }
+    while(stack.length>0){
+      suffixExp+=stack.pop()
+    }
+
+    //后缀表达式计算结果
+    let stack1 = []
+    for(let v of suffixExp) {
+      console.log(v)
+    }
+
   }
 
 })();
