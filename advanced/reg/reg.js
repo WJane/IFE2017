cce34aed36ccfdc944a02ec6225508181f74887c
Jane Wu <1198829740@qq.com>
2017-05-19 19:12:38 +0800

    img

--- advanced/reg/reg.js
+++ advanced/reg/reg.js
@@ -2,36 +2,80 @@ window.onload = function() {
   var textarea = document.getElementById('text');
   var content;
   var show = document.getElementById('show')
+  var memory = show.innerHTML
   textarea.oninput = function() {
     content = textarea.value
 
     show.innerHTML = marked(content)
+
   }
 }
 
 var exps = {
   head:  /^\s*(#{1,6})\s*([^\n]+?)\s*#*\s*(?:\n+|$)/,
-  newline: /.*\n/,
-  stalic:  /\*([^*]+?)\*/gm
-
+  // newline: /.*\n/,
+  stalic:  /\*(.*)\*/gm,
+  code: /`(.*)`/,
+  strong: /\*\*(.*)\*\*/gm,
+  through: /~~(.*)~~/gm,
+  link: /\[(.*)\]\((.*)\)/,
+  img: /!\[img\]\((.*)\)/
 }
 
 function marked(content) {
-  var result
-  console.log(typeof exps.head)
+  var result = ''
+
   if(exps.head.test(content)){
-    var extract = exps.head.exec(content)
+    var extract = /^\s*(#{1,6})\s*([^\n]+?)\s*#*\s*(?:\n+|$)/.exec(content)
     var count = extract[1].length;
-    result = '<h'+count+'>'+extract[2]+'</h'+count+'>'
+    result += '<h'+count+'>'+extract[2]+'</h'+count+'>'
 
   }
 
   if(exps.stalic.test(content)){
-    var temp = exps.stalic.exec(content)[1]
-    result = '<i>'+temp+'</i>'
-    console.log('s')
+    var extract = /\*(.*)\*/gm.exec(content)[1]
+    var rep = '<i>'+extract+'</i>'
+    var end =  content.replace(/\*(.*)\*/gm, rep)
+    result+=end
+
   }
-  console.log(result)
+
+  if (exps.code.test(content)) {
+    var c = /`(.*)`/.exec(content)[1]
+    var rep = '<code>'+c+'</code>'
+    var end = content.replace(/`(.*)`/g, rep)
+    result += end
+  }
+
+
+  if (exps.strong.test(content)) {
+    var extract = /\*\*([^*]+?)\*\*/gm.exec(content)[1]
+    var rep = '<strong>'+extract+'</strong>'
+    var end =  content.replace(/\*\*([^*]+?)\*\*/g, rep)
+    result+=end
+  }
+
+  if (exps.through.test(content)) {
+    var extract = /~~(.*)~~/gm.exec(content)[1]
+    var rep = '<del>'+extract+'</del>'
+    var end =  content.replace(/~~(.*)~~/gm, rep)
+    result+=end
+  }
+
+  if(exps.link.test(content)) {
+    var extract = /\[(.*)\]\((.*)\)/.exec(content)[1]
+    var distress = /\[(.*)\]\((.*)\)/.exec(content)[2]
+    var rep = '<a href="'+ distress + '">'+ extract +'</a>'
+    var end = content.replace(/\[(.*)\]\((.*)\)/g, rep)
+    result += end
+  }
+
+  if(exps.img.test(content)) {
+    var extract = /!\[img\]\((.*)\)/.exec(content)[1]
+    var rep = '<img src="' + extract + '" />'
+    var end = content.replace(/!\[img\]\(.*\)/g, rep)
+    result+=end
+  }
   return result;
 
   // if(exps.newline.test(content)){
