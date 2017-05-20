fdd846f07e9e1911d1c1e5cf8d135ae449b46ade
Jane Wu <1198829740@qq.com>
2017-05-20 12:35:58 +0800

    list has bugs

--- advanced/reg/reg.js
+++ advanced/reg/reg.js
@@ -3,6 +3,7 @@ window.onload = function() {
   var content;
   var show = document.getElementById('show')
   var memory = show.innerHTML
+  var ul = document.createElement('ul')
   textarea.oninput = function() {
     content = textarea.value
 
@@ -19,12 +20,24 @@ var exps = {
   strong: /\*\*(.*)\*\*/gm,
   through: /~~(.*)~~/gm,
   link: /\[(.*)\]\((.*)\)/,
-  img: /!\[img\]\((.*)\)/
+  img: /!\[img\]\((.*)\)/,
+  // pre: /```(.*)```/
+  ul: /\-\s\w+\n+/
 }
+
+// function test(){
+//   var re = /\n*\-\s(\w+|\s+)\n*$/g
+//   var r = 0
+//   var s = '- sdfsdfs\n- sdfsdfsd\n -sdfsd\n\n- ppp'
+//   while(r = re.exec(s)){
+
+//   }
+// }
 
 function marked(content) {
   var result = ''
 
+  //斜体
   if(exps.head.test(content)){
     var extract = /^\s*(#{1,6})\s*([^\n]+?)\s*#*\s*(?:\n+|$)/.exec(content)
     var count = extract[1].length;
@@ -32,6 +45,7 @@ function marked(content) {
 
   }
 
+  //斜体
   if(exps.stalic.test(content)){
     var extract = /\*(.*)\*/gm.exec(content)[1]
     var rep = '<i>'+extract+'</i>'
@@ -40,14 +54,15 @@ function marked(content) {
 
   }
 
-  if (exps.code.test(content)) {
-    var c = /`(.*)`/.exec(content)[1]
-    var rep = '<code>'+c+'</code>'
-    var end = content.replace(/`(.*)`/g, rep)
-    result += end
-  }
+  // if (exps.code.test(content)) {
+  //   var c = /`(.*)`/.exec(content)[1]
+  //   var rep = '<code>'+c+'</code>'
+  //   var end = content.replace(/`(.*)`/g, rep)
+  //   result += end
+  // }
 
 
+  //加粗
   if (exps.strong.test(content)) {
     var extract = /\*\*([^*]+?)\*\*/gm.exec(content)[1]
     var rep = '<strong>'+extract+'</strong>'
@@ -55,6 +70,7 @@ function marked(content) {
     result+=end
   }
 
+  //删除线
   if (exps.through.test(content)) {
     var extract = /~~(.*)~~/gm.exec(content)[1]
     var rep = '<del>'+extract+'</del>'
@@ -62,7 +78,8 @@ function marked(content) {
     result+=end
   }
 
-  if(exps.link.test(content)) {
+  //链接
+  if (exps.link.test(content)) {
     var extract = /\[(.*)\]\((.*)\)/.exec(content)[1]
     var distress = /\[(.*)\]\((.*)\)/.exec(content)[2]
     var rep = '<a href="'+ distress + '">'+ extract +'</a>'
@@ -70,12 +87,45 @@ function marked(content) {
     result += end
   }
 
-  if(exps.img.test(content)) {
+  //图片
+  if (exps.img.test(content)) {
     var extract = /!\[img\]\((.*)\)/.exec(content)[1]
     var rep = '<img src="' + extract + '" />'
     var end = content.replace(/!\[img\]\(.*\)/g, rep)
     result+=end
   }
+
+  // if (exps.pre.test(content)) {
+  //   var extract = /```(.*)```/.exec(content)[1]
+  //   var rep = '<pre>'+extract+'</pre>'
+  //   var end = content.replace(/```(.*)```/g, rep)
+  //   result += end
+  // }
+
+  //无序列表
+  if(/\n*\-\s(\w+|\s+)\n*$/.test(content)) {
+    var extract = /\n*\-\s(\w+|\s+)\n*$/.exec(content)[1]
+    var memory = document.getElementById('show').innerHTML
+    var li = '<li>'+extract+'</li>'
+    var item = content.replace(/\n*\-\s(\w+|\s+)\n*$/, li)
+
+
+    result+=item
+
+  }
+
+  //有序列表
+  if(/\n*\d\.(\w+|\s+)\n*$/.test(content)) {
+    var extract = /\n*\d\.(\w+|\s+)\n*$/.exec(content)[1]
+    var memory = document.getElementById('show').innerHTML
+    var li = '<li>'+extract+'</li>'
+    var item = content.replace(/\n*\d\.(\w+|\s+)\n*$/, li)
+
+
+    result+=item
+
+  }
+
   return result;
 
   // if(exps.newline.test(content)){
